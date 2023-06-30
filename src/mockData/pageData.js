e568a6f54a3fc925afce46dd9adf8bbdaccaec40
guoguangkun <guoguangkun1024@gmail.com>
2023-06-30 12:07:49 +0800

    static file

---
+++ src/mockData/pageData.js
@@ -0,0 +1,132 @@
+export default {
+    "cards": [
+        {   
+            "id": 1, 
+            "title": "Introduction to programming",
+            // 1  started
+            "status": 1, 
+        
+            "desc": "Introduction to programming",
+            "tags": [
+                {
+                    "label": "Beginner",
+                    "id": "Beginner"
+                }
+            ],
+            "content": "This course covers the most basic concepts in programming using Solidity as an example.",
+            "required": {
+                "time": "36 Hour",
+                "course": "5 Course"
+            },
+            "classInfo":[
+                {
+                    key: "time",
+                    value: "36 Hour",
+                },
+                {
+                    key: "course",
+                    value: "5 Course",
+                }
+            ],
+            "schedule": "45% COMPLETED"
+
+        },
+        {   
+            "id":2, 
+            "title": "Moonshot 2023 Summer Hackathon",
+            // 1  pre
+            "status": 2,
+            "desc": "",
+            "tags": [
+                {
+                    "label": "All Tracks",
+                    "id": "All Tracks"
+                },
+                {
+                    "label": "Solidity",
+                    "id": "Solidity"
+                },
+                {
+                    "label": "ZK",
+                    "id": "ZK"
+                }
+            ],
+            "content": "",
+            "required": {
+                "time": "36 Hour",
+                "course": "5 Course"
+            },
+            "classInfo":[
+        
+            ],
+            "schedule": "",
+            "info": [
+                {
+                    key: "signUp",
+                    value: '4/15 - 6/15'
+                },
+                {
+                    key: "Event",
+                    value: '6/15 - 7/15'
+                },
+                {
+                    key: "Grant size",
+                    value: '200K'
+                },
+            ]
+        },
+        {   
+            "id":3, 
+            "title": "Web 3.0 Programming Basics",
+             // 3  signUp end ready to start
+            "status": 3,
+            "desc": "Web 3.0 Programming Basics",
+            "tags": [
+                {
+                    "label": "Beginner",
+                    "id": "Beginner"
+                }
+            ],
+            "content": "Basic concepts in programming of Solidity. Topics include: variables, functions, flow control, error handling, data structure.",
+            "required": {
+                "time": "36 Hour",
+                "course": "5 Course"
+            },
+            "classInfo":[
+                {
+                    key: "time",
+                    value: "36 Hour",
+                },
+                {
+                    key: "course",
+                    value: "5 Course",
+                }
+            ],
+            "schedule": ""
+        },
+        {   
+            "id":4, 
+            "title": "",
+             // 4 finished or Just ad
+            "status": 4,
+            // "ad": 'What is Bitcoin',
+            "desc": "What is Bitcoin",
+            "adTitle": "What is Bitcoin",
+            "tags": [
+                
+            ],
+            "content": "",
+            "classInfo":[
+                {
+                    key: "time",
+                    value: "36 Hour",
+                }
+            ],
+            "required": {
+                "time": "36 Hour",
+                "course": "5 Course"
+            },
+            "schedule": ""
+        }
+    ]
+}
